c732a0d0240d32def9950cdc1b0a1903c7168d7c
anandkay321 <117114098+anandkay321@users.noreply.github.com>
2022-11-18 13:16:09 -0500

    Add files via upload

--- encoder.py
+++ encoder.py
@@ -5,8 +5,12 @@ class encode:
     __init__():
     ''' '''   
     word_checker():
-    
-    word_input():   
+    '''This funtion's purpose is to check if the user inputed word can be encoded
+    if it reads the string and it contains anything other than letter it will return
+    'word can not be encoded' '''
+    word_input():
+    '''This funtion's purpose is what the user will use to input their words that they
+    choose to encode'''   
     
     file_encoding():
     
