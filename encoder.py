cb288e33df5687d19986fa91ed8b5b7c6df33402
joeypooch <58953076+joeypooch@users.noreply.github.com>
2022-12-12 18:51:10 -0500

    Update encoder.py

--- encoder.py
+++ encoder.py
@@ -52,24 +52,109 @@ class encode:
 #Joe
     def __repr__(self):
         return(f"encod('{self.plain_text}')")
-    
+     
 class decode(encod):
-    def __decryp__(text, shift*-1, alphabets):
-        return super().__cryp__(text, shift*-1, alphabets)
-    
-choose = 0
-while choose != 1 and choose != 0:
-    choose = input("Encode(1) or Decode(2):")
-    if choose == 1:
-        x = encod(input("Enter string: "))
-        plain_text = x.word_checker(x.plain_text)
-        coded_word = f' plain text, {plain_text} has been encrypted to {encod.__cryp__(plain_text, 7, [string.ascii_lowercase, string.ascii_uppercase, string.punctuation])}'
-        print(coded_word) 
-    elif choose == 2:
-        y = decode(input("Enter string: "))
-        encoded_text = y.word_checker(y.plain_text)
-        print(f' encoded text, {encoded_text} has been decrypted to {decode.__decryp__(plain_text, 7, [string.ascii_lowercase, string.ascii_uppercase, string.punctuation])}')
+    def cryp(self, text, shift, alphabets):
+        return super().cryp(text, shift*-1, alphabets)
 
+def file_encoder(path, shift, e_or_d):
+    '''Joe's function
+    Content: 
+        with statements
+    Description:
+        In the case a file is specified to be edited, this method opens it,
+        takes in the content, encodes it, and shifts it as shift describes
+    Args:
+        Path(str): Path to file to be encoded
+        Shift(int): Amount to shift the letters in the file by
+        e_or_d(char): 1 if encoding, 2 if decoding, indicates which to do
+    Returns: 
+        N/A
+    Side Effects:
+        Opens and edits file specified in path argument
+        Creates encod object
+    '''
+    
+    # Opens file and reads line into data list
+    data = []
+    f = open(path, 'r')
+    data = f.readlines()
+    
+    f.close()
+    #If encode selected, opens file, encodes line, and writes that to file
+    if(e_or_d == "1"):
+        with open(path, mode= 'w') as f: 
+            for x in data:
+                e = encod(x)
+                f.write(e.cryp(e.plain_text, int(shift), ALPHABET))
+    #If decode selected, opens file, decodes line, and writes that to file            
+    else:
+        with open(path, mode= 'w') as f:
+            for x in data:
+                e = decode(x)
+                f.write(e.cryp(e.plain_text, int(shift), ALPHABET))
+
+def main(e_or_d =  '-1' ,path = None):
+    '''Joe's function
+    Content: 
+        Optional parameters
+    Description: 
+        Actually runs the program, giving the user options in how they
+    want it to operate. 
+    Args:
+        e_or_d(char): Specifies whether to encode or decode
+            '1' = endode
+            '2' = decode
+            Defaults to -1 to ensure trigger of selection prompt
+        path(str): 
+            optional arg to path to file to be edited
+            Defaults to none so if not provided, will skip to raw input prompt
+        If a .txt file is the only argument provided, the method will detect 
+        that, switch that to the path, and prompt the user for a enocde or
+        decode selection
+    Returns: 
+        N/A
+    Side Effects:
+        Prints input prompts to console
+        Can alter e_or_d, a parameter
+        Creates encod or decode objects
+    '''
+    # Detects if arg in wrong spot
+    if(e_or_d.__contains__(".txt")):
+        path = e_or_d
+        e_or_d = '-1'
+        
+    if(path != None and path.__contains__(".txt") == False):
+        while path.__contains__(".txt") == False:
+            print("invalid file given, please put a .txt file in")
+            path = input("Input .txt file:")
+    #If invalid selection, prompts user for encode or decode selection
+    if(e_or_d != '1' and e_or_d != '2'):
+        while e_or_d != '1' and e_or_d != '2':
+            e_or_d = input("Encode(1) or Decode(2):")
+    #If no path given, reads in string to encode or decode
+    if(path== None):
+        if e_or_d == '1':            
+            x = input("Enter string to encode: ")
+            y = input("Enter shift to encode by:")
+            e = encod(x)
+            x = e.word_checker        
+            z = e.cryp(e.plain_text, int(y), alphabets=[string.ascii_lowercase, string.ascii_uppercase, string.punctuation])
+            print(f"Encoded text: {z}")
+                    
+        elif(e_or_d == '2'):                  
+            d = decode(input("Enter string to decode: "))
+            y = int(input("Enter integer used to encode message:"))
+            z = d.cryp(d.plain_text,y,[string.ascii_lowercase, string.ascii_uppercase, string.punctuation])
+            print(f"Decoded text: {z}")
+    #Encode or decode .txt file 
+    else: 
+        if(e_or_d == "1"):
+            shift = input("Enter shift for file encoding:")
+        else:
+            shift = input("Enter shift for file decoding:")
+        file_encoder(path, shift, e_or_d)
+        print("File successfully encoded")
     
 def parse_args():
     """Parse and validate command line arguments.
@@ -84,3 +169,14 @@ def parse_args():
     Returns:
         namespace: the parsed argument
     """
+if __name__ == "__main__":
+    '''
+    Joe 
+    Parses arguments, checks how many there are, and initializes main with the
+    requisite amount of args
+    '''
+    args = parse_args(sys.argv[1:])
+    if(len(sys.argv) > 1):
+        main(args.e_or_d, args.path)
+    else:
+        main(args.e_or_d)
